880d404cf3439ec055070fd2d5500a969f761e03
Walter Chen <cicidi@users.noreply.github.com>
2020-02-16 19:13:02 -0800

    pipeline.go

---
+++ pipeline.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	test0()
+	test1()
+	test2()
+}
+
+func test0() {
+	// Set up the pipeline.
+	fmt.Println("test")
+	c := gen(2, 3)
+	out := sq(c)
+
+	// Consume the output.
+	fmt.Println(<-out) // 4
+	fmt.Println(<-out) // 9
+
+}
+func test1() {
+	fmt.Println("test1")
+	in := gen(2, 3)
+	// Distribute the sq work across two goroutines that both read from in.
+	c1 := sq(in)
+	c2 := sq(in)
+
+	// Consume the merged output from c1 and c2.
+	for n := range merge(c1, c2) {
+		fmt.Println(n) // 4 then 9, or 9 then 4
+	}
+}
+
+func test2() {
+	// Set up the pipeline and consume the output.
+
+	fmt.Println("test2")
+	for n := range sq(sq(gen(2, 3))) {
+		fmt.Println(n) // 16 then 81
+	}
+}
+func sq(in <-chan int) <-chan int {
+	out := make(chan int)
+	go func() {
+		for n := range in {
+			out <- n * n
+		}
+		close(out)
+	}()
+	return out
+}
+
+func gen(nums ...int) <-chan int {
+	out := make(chan int)
+	go func() {
+		for _, n := range nums {
+			out <- n
+		}
+		close(out)
+
+	}()
+	return out
+}
+
+func merge(cs ...<-chan int) <-chan int {
+	var wg sync.WaitGroup
+	out := make(chan int)
+
+	// Start an output goroutine for each input channel in cs.  output
+	// copies values from c to out until c is closed, then calls wg.Done.
+	output := func(c <-chan int) {
+		for n := range c {
+			out <- n
+		}
+		wg.Done()
+	}
+	wg.Add(len(cs))
+	for _, c := range cs {
+		go output(c)
+	}
+
+	// Start a goroutine to close out once all the output goroutines are
+	// done.  This must start after the wg.Add call.
+	go func() {
+		wg.Wait()
+		close(out)
+	}()
+	return out
+}
